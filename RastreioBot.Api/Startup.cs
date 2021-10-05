c31ddc37e5f9a1df7015fb9f6765c510c70866bc
mtswill <matheuswillian91@hotmail.com>
2021-10-05 20:37:10 -0300

    using postgres

--- RastreioBot.Api/Startup.cs
+++ RastreioBot.Api/Startup.cs
@@ -25,7 +25,8 @@ namespace RastreioBot.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BotContext>(options => options.UseSqlite("DataSource=RastreioBot.db"));
+            var connectionString = Configuration.GetConnectionString("RastreioBot");
+            services.AddDbContext<BotContext>(options => options.UseNpgsql(connectionString));
 
             #region DI
 
